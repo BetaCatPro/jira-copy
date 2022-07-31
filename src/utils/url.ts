0a4131cd361cdf9b8840460d5c2eb50acb6fddf9
BetaCatPro <zj20162325@163.com>
2022-07-31 15:35:15 +0800

    update kanban view

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,29 +1,37 @@
-import { URLSearchParamsInit, useSearchParams } from 'react-router-dom';
-import { useMemo } from 'react';
-import { cleanObject, subset } from 'utils/index';
+import { URLSearchParamsInit, useSearchParams } from "react-router-dom";
+import { useMemo, useState } from "react";
+import { cleanObject, subset } from "utils/index";
 
 /**
  * 返回页面url中，指定键的参数值
  */
 export const useUrlQueryParam = <K extends string>(keys: K[]) => {
-    const [searchParams, setSearchParam] = useSearchParams();
-    return [
-        useMemo(
-            () =>
-        subset(Object.fromEntries(searchParams), keys) as {
+  const [searchParams] = useSearchParams();
+  const setSearchParams = useSetUrlSearchParam();
+  const [stateKeys] = useState(keys);
+  return [
+    useMemo(
+      () =>
+        subset(Object.fromEntries(searchParams), stateKeys) as {
           [key in K]: string;
         },
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-            [searchParams],
-        ),
-        (params: Partial<{ [key in K]: unknown }>) => {
-            // iterator
-            // iterator: https://codesandbox.io/s/upbeat-wood-bum3j?file=/src/index.js
-            const o = cleanObject({
-                ...Object.fromEntries(searchParams),
-                ...params,
-            }) as URLSearchParamsInit;
-            return setSearchParam(o);
-        },
-    ] as const;
+      [searchParams, stateKeys]
+    ),
+    (params: Partial<{ [key in K]: unknown }>) => {
+      return setSearchParams(params);
+      // iterator
+      // iterator: https://codesandbox.io/s/upbeat-wood-bum3j?file=/src/index.js
+    },
+  ] as const;
+};
+
+export const useSetUrlSearchParam = () => {
+  const [searchParams, setSearchParam] = useSearchParams();
+  return (params: { [key in string]: unknown }) => {
+    const o = cleanObject({
+      ...Object.fromEntries(searchParams),
+      ...params,
+    }) as URLSearchParamsInit;
+    return setSearchParam(o);
+  };
 };
